c3a0c8d0b22c8cd154354cf4361773767199b634
developerjulian714 <developerjulian714@gmail.com>
2023-01-13 00:00:00 +0530

    feat: setup project structure, types, and design system

---
+++ types/index.ts
@@ -0,0 +1,46 @@
+export type Role = 'ADMIN' | 'MEMBER';
+
+export interface User {
+    id: string;
+    name: string;
+    avatar?: string; // URL or Initials
+}
+
+export interface Member extends User {
+    role: Role;
+    joinedAt: string;
+}
+
+export interface Family {
+    id: string;
+    name: string;
+    members: Member[];
+    createdAt: string;
+}
+
+export type SplitType = 'EQUAL' | 'CUSTOM' | 'EXACT';
+
+export interface Split {
+    userId: string;
+    amount: number;
+}
+
+export interface Expense {
+    id: string;
+    description: string;
+    amount: number;
+    paidBy: string; // userId
+    date: string;
+    category: string;
+    splits: Split[];
+    familyId: string;
+    createdAt: string;
+}
+
+export interface Budget {
+    id: string;
+    familyId: string;
+    category: string;
+    limit: number;
+    month: string; // YYYY-MM
+}
