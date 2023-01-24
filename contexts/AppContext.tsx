f86ee0ddf8d72f731c7405ddca1df2b0d81c0e7d
developerjulian714 <developerjulian714@gmail.com>
2023-01-25 00:00:00 +0530

    feat: implement persistence layer and app context

---
+++ contexts/AppContext.tsx
@@ -0,0 +1,142 @@
+"use client";
+
+import React, { createContext, useContext, useState } from 'react';
+import { useLocalStorage } from '@/hooks/use-local-storage';
+import { Family, Expense, Budget, Member, Role } from '@/types';
+import { INITIAL_FAMILIES, INITIAL_EXPENSES, INITIAL_BUDGETS } from '@/lib/dummy-data';
+
+interface AppContextType {
+    families: Family[];
+    expenses: Expense[];
+    budgets: Budget[];
+    currentUser: Member | null;
+    activeFamily: Family | null;
+
+    // Actions
+    addExpense: (expense: Omit<Expense, 'id' | 'createdAt'>, splits: Expense['splits']) => void;
+    addFamily: (name: string) => void;
+    addMember: (familyId: string, name: string, role: Role) => void;
+    switchUser: (userId: string) => void;
+    setActiveFamilyId: (familyId: string) => void;
+}
+
+const AppContext = createContext<AppContextType | undefined>(undefined);
+
+export function AppProvider({ children }: { children: React.ReactNode }) {
+    // Persisted Users/Families
+    const [families, setFamilies] = useLocalStorage<Family[]>('families', INITIAL_FAMILIES);
+    const [expenses, setExpenses] = useLocalStorage<Expense[]>('expenses', INITIAL_EXPENSES);
+    const [budgets] = useLocalStorage<Budget[]>('budgets', INITIAL_BUDGETS);
+
+    // Session State (not persisted necessarily, but good for navigation)
+    const [activeFamilyId, setActiveFamilyIdState] = useLocalStorage<string>('activeFamilyId', INITIAL_FAMILIES[0].id);
+    const [currentUserId, setCurrentUserId] = useLocalStorage<string>('currentUserId', INITIAL_FAMILIES[0].members[0].id);
+
+    // Derived State
+    const activeFamily = families.find(f => f.id === activeFamilyId) || families[0];
+
+    const currentUser = (() => {
+        for (const f of families) {
+            const m = f.members.find(u => u.id === currentUserId);
+            if (m) return m;
+        }
+        return activeFamily.members[0] || null;
+    })();
+
+    const addExpense = (expenseData: Omit<Expense, 'id' | 'createdAt'>, splits: Expense['splits']) => {
+        const newExpense: Expense = {
+            ...expenseData,
+            id: `exp_${Date.now()}`,
+            createdAt: new Date().toISOString(),
+            splits
+        };
+        setExpenses(prev => [newExpense, ...prev]);
+    };
+
+    const addFamily = (name: string) => {
+        const newFamily: Family = {
+            id: `fam_${Date.now()}`,
+            name,
+            createdAt: new Date().toISOString(),
+            members: [
+                {
+                    id: `u_${Date.now()}`,
+                    name: 'Admin',
+                    role: 'ADMIN',
+                    joinedAt: new Date().toISOString(),
+                    avatar: 'AD'
+                }
+            ]
+        };
+        setFamilies(prev => [...prev, newFamily]);
+        setActiveFamilyIdState(newFamily.id);
+    };
+
+    const addMember = (familyId: string, name: string, role: Role) => {
+        setFamilies(prev => prev.map(fam => {
+            if (fam.id === familyId) {
+                return {
+                    ...fam,
+                    members: [
+                        ...fam.members,
+                        {
+                            id: `u_${Date.now()}_${Math.random().toString(36).substr(2, 9)}`,
+                            name,
+                            role,
+                            joinedAt: new Date().toISOString(),
+                            avatar: name.slice(0, 2).toUpperCase()
+                        }
+                    ]
+                };
+            }
+            return fam;
+        }));
+    };
+
+    const switchUser = (userId: string) => {
+        setCurrentUserId(userId);
+    };
+
+    const setActiveFamilyId = (id: string) => {
+        setActiveFamilyIdState(id);
+    };
+
+    // Prevent hydration mismatch by rendering null until mounted if needed, 
+    // but useLocalStorage handles strictly client-side via useState default which is safe-ish, 
+    // though typically you want a mounted check.
+    const [isMounted, setIsMounted] = useState(false);
+    React.useEffect(() => {
+        setIsMounted(true);
+    }, []);
+
+    if (!isMounted) {
+        return null; // Or a loader
+    }
+
+    return (
+        <AppContext.Provider
+            value={{
+                families,
+                expenses,
+                budgets,
+                currentUser,
+                activeFamily,
+                addExpense,
+                addFamily,
+                addMember,
+                switchUser,
+                setActiveFamilyId
+            }}
+        >
+            {children}
+        </AppContext.Provider>
+    );
+}
+
+export function useAppContext() {
+    const context = useContext(AppContext);
+    if (context === undefined) {
+        throw new Error('useAppContext must be used within a AppProvider');
+    }
+    return context;
+}
---
+++ lib/dummy-data.ts
@@ -0,0 +1,97 @@
+import { Family, Expense, Budget } from '@/types';
+
+export const INITIAL_FAMILIES: Family[] = [
+    {
+        id: 'fam_1',
+        name: "The Sharma's",
+        createdAt: new Date().toISOString(),
+        members: [
+            {
+                id: 'u_1',
+                name: 'Rahul Sharma',
+                role: 'ADMIN',
+                joinedAt: new Date().toISOString(),
+                avatar: 'RS',
+            },
+            {
+                id: 'u_2',
+                name: 'Priya Sharma',
+                role: 'MEMBER',
+                joinedAt: new Date().toISOString(),
+                avatar: 'PS',
+            },
+            {
+                id: 'u_3',
+                name: 'Amit Sharma',
+                role: 'MEMBER',
+                joinedAt: new Date().toISOString(),
+                avatar: 'AS',
+            },
+        ],
+    },
+];
+
+export const INITIAL_EXPENSES: Expense[] = [
+    {
+        id: 'exp_1',
+        description: 'Groceries from BigBasket',
+        amount: 1540,
+        paidBy: 'u_1',
+        date: new Date(Date.now() - 86400000 * 2).toISOString(), // 2 days ago
+        category: 'Groceries',
+        familyId: 'fam_1',
+        createdAt: new Date().toISOString(),
+        splits: [
+            { userId: 'u_1', amount: 513 },
+            { userId: 'u_2', amount: 513 },
+            { userId: 'u_3', amount: 514 },
+        ],
+    },
+    {
+        id: 'exp_2',
+        description: 'Electricity Bill',
+        amount: 3200,
+        paidBy: 'u_2',
+        date: new Date(Date.now() - 86400000 * 5).toISOString(), // 5 days ago
+        category: 'Utilities',
+        familyId: 'fam_1',
+        createdAt: new Date().toISOString(),
+        splits: [
+            { userId: 'u_1', amount: 1066 },
+            { userId: 'u_2', amount: 1067 },
+            { userId: 'u_3', amount: 1067 },
+        ],
+    },
+    {
+        id: 'exp_3',
+        description: 'Weekend Pizza Party',
+        amount: 1800,
+        paidBy: 'u_3',
+        date: new Date(Date.now() - 86400000).toISOString(), // Yesterday
+        category: 'Eating Out',
+        familyId: 'fam_1',
+        createdAt: new Date().toISOString(),
+        splits: [
+            { userId: 'u_1', amount: 600 },
+            { userId: 'u_2', amount: 600 },
+            { userId: 'u_3', amount: 600 },
+        ],
+    },
+];
+
+export const INITIAL_BUDGETS: Budget[] = [
+    {
+        id: 'bud_1',
+        familyId: 'fam_1',
+        category: 'Groceries',
+        limit: 15000,
+        month: new Date().toISOString().slice(0, 7), // Current month YYYY-MM
+    },
+    {
+        id: 'bud_2',
+        familyId: 'fam_1',
+        category: 'Eating Out',
+        limit: 5000,
+        month: new Date().toISOString().slice(0, 7),
+    },
+];
