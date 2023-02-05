2adb117bcd7e145ce88c0e1de07118d414702480
developerjulian714 <developerjulian714@gmail.com>
2023-02-05 14:00:00 +0530

    feat: add route transition progress bar

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Outfit } from 'next/font/google';
 import './globals.css';
 import { cn } from '@/lib/utils';
 import { AppProvider } from '@/contexts/AppContext';
+import NextTopLoader from 'nextjs-toploader';
 
 const outfit = Outfit({
   subsets: ['latin'],
@@ -22,6 +23,17 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={cn('min-h-screen bg-background font-sans text-foreground antialiased', outfit.variable)}>
+        <NextTopLoader
+          color="#6366f1"
+          initialPosition={0.08}
+          crawlSpeed={200}
+          height={3}
+          crawl={true}
+          showSpinner={false}
+          easing="ease"
+          speed={200}
+          shadow="0 0 10px #6366f1,0 0 5px #6366f1"
+        />
         <AppProvider>
           {children}
         </AppProvider>
