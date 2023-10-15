45b316e9f16c7e2e9d8d59e57c87870ea4848945
developerjulian714 <developerjulian714@gmail.com>
2023-10-16 00:00:00 +0530

    style: fix Tailwind v4 lint in Toast.tsx

--- components/ui/Toast.tsx
+++ components/ui/Toast.tsx
@@ -8,7 +8,7 @@ export function ToastContainer() {
     const { toasts, hideToast } = useToast();
 
     return (
-        <div className="fixed bottom-6 right-6 z-[100] flex flex-col gap-3 pointer-events-none">
+        <div className="fixed bottom-6 right-6 z-100 flex flex-col gap-3 pointer-events-none">
             {toasts.map((toast) => (
                 <div
                     key={toast.id}
